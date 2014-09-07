a31e4cc71fa679b504f2fafa4aae517cf1e5ba1c
Sergio Tanzilli <tanzilli@acmesystems.it>
2014-09-07 22:13:14 +0200

    Linux path

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-make -C ~/linux-3.16.1 ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- M=`pwd` modules
+make -C ~/arietta/linux-3.16.1 ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- M=`pwd` modules
 sshpass -p acmesystems scp ledpanel.ko root@192.168.10.10:/root
