b122bde05711e44438a556543e28b55094beef4f
Sergio Tanzilli <tanzilli@acmesystems.it>
2014-09-08 15:16:54 +0200

    Updated fullcolor.c

--- fullcolor.c
+++ fullcolor.c
@@ -10,6 +10,7 @@
 #define MAXBUFFER_PER_PANEL 32*32*3
 #define OUT_FILE "/sys/class/ledpanel/rgb_buffer"
 
+// Write on rgb_buffer the buffer content
 void WriteBuffer(unsigned char *buffer) {
 	int fd;
 
@@ -17,12 +18,12 @@ void WriteBuffer(unsigned char *buffer) {
 		printf("open() error\n");
 		return;
 	}
-	
 	write(fd,buffer,MAXBUFFER_PER_PANEL);
 	close(fd);
 }
 
-void Full(unsigned char r,unsigned char g,unsigned char b,unsigned char *buffer, int show) {
+// Fill the buffer with a rgb color
+void fill_full(unsigned char r,unsigned char g,unsigned char b,unsigned char *buffer, int show) {
 	int i;
 	
 	for (i=0;i<MAXBUFFER_PER_PANEL;i+=3) {
@@ -33,37 +34,17 @@ void Full(unsigned char r,unsigned char g,unsigned char b,unsigned char *buffer,
 	if (show) WriteBuffer(buffer);
 }
 
-#define PAUSE 500000 //us
-
-void main(void) {
-	int i;
+void main(int argc, char *argv[]) {
+	int i,r,g,b;
 	unsigned char buffer[MAXBUFFER_PER_PANEL];
-	
-	Full(0,0,0,buffer,0);	
 
-	for (i=0;i<100;i++) {
-		// RED
-		Full(1,0,0,buffer,1);	
-		usleep(PAUSE);
-
-		// GREEN
-		Full(0,1,0,buffer,1);	
-		usleep(PAUSE);
-
-		// BLUE
-		Full(0,0,1,buffer,1);	
-		usleep(PAUSE);
-
-		// YELLOW
-		Full(1,1,0,buffer,1);	
-		usleep(PAUSE);
-
-		// CYAN
-		Full(0,1,1,buffer,1);	
-		usleep(PAUSE);
-
-		// WHITE
-		Full(1,1,1,buffer,1);	
-		usleep(PAUSE);
-	}
+    if (argc!=4) {
+        printf( "Use: %s r g b\n", argv[0] );
+    } else {
+		fill_full(0,1,0,buffer,1);
+		r=atoi(argv[1]);
+		g=atoi(argv[2]);
+		b=atoi(argv[3]);
+		fill_full(r,g,b,buffer,1);
+	}		
 }
