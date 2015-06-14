e76a5a8731ff11a467ad7c591947f891dd37e3d3
Sergio Tanzilli <tanzilli@acmesystems.it>
2015-06-14 09:57:03 +0200

    Licence info and new name

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-#Simple Linux driver for a RGB led panel
+#Simple Linux driver for a 32x32 RGB led panel
 
-Bit banging driver to manage one RGB 32x32 led panel with an 
+Bit banging driver to manage a RGB led panel using the  
 [Arieta G25 Linux Embedded SoM](http://www.acmesystems.it/arietta)
 
 Project documentation:
 
-* [32x32 RGB Led Panel home page](http://www.acmesystems.it/ledpanel)
+* [Led Panel home page](http://www.acmesystems.it/ledpanel)
 
-Video demo:
+Video:
 
 * [Sliding clock](http://www.youtube.com/embed/Qszwey7jYl4)
 
--- ledpanel.c
+++ ledpanel.c
@@ -1,4 +1,22 @@
-#include <linux/init.h>
+/*
+ * linux/driver/ledpanel/ledpanel.c
+ * 
+ * Bit banging Linux driver for RGB 32x32 led panel
+ * Optimized version with brightness control
+ * 
+ *
+ * Copyright (C) 2015 Sergio Tanzilli, All Rights Reserved.
+ *
+ * sergio@tanzilli.com 
+ * http://www.acmesystems.it/ledpanel 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ * 
+ */
+
+#include <linux/init.h> 
 #include <linux/module.h>
 #include <linux/gpio.h>
 #include <linux/delay.h>
@@ -10,20 +28,26 @@
 #include <linux/ktime.h>
 #include <linux/device.h>
 #include <linux/kdev_t.h>
+#include <linux/irqflags.h>
 
-#define LEDPANEL_R0		0 
-#define LEDPANEL_G0		1 
-#define LEDPANEL_B0		2 
-#define LEDPANEL_R1		3 
-#define LEDPANEL_G1		4 
-#define LEDPANEL_B1		5 
-#define LEDPANEL_A		6 
-#define LEDPANEL_B		7 
-#define LEDPANEL_C		8 
-#define LEDPANEL_D		9 
-#define LEDPANEL_CLK	10 
-#define LEDPANEL_STB	11 
-#define LEDPANEL_OE		12 
+#include <asm/io.h>
+#include <mach/at91_pio.h>
+#include <mach/hardware.h>
+
+#define LEDPANEL_R0		0
+#define LEDPANEL_G0		1
+#define LEDPANEL_B0		2
+#define LEDPANEL_R1		3
+#define LEDPANEL_G1		4
+#define LEDPANEL_B1		5
+#define LEDPANEL_A		6
+#define LEDPANEL_B		7
+#define LEDPANEL_C		8
+#define LEDPANEL_D		9
+#define LEDPANEL_CLK	10
+#define LEDPANEL_STB	11
+#define LEDPANEL_OE		12
+#define LEDPANEL_TP		13
 
 #define MAXBUFFER_PER_PANEL 32*32*3
 
@@ -31,18 +55,90 @@ MODULE_LICENSE("Dual BSD/GPL");
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Sergio Tanzilli");
-MODULE_DESCRIPTION("Driver for RGB 32x32 LED PANEL");
+MODULE_DESCRIPTION("Bit banging driver for RGB 32x32 LED PANEL");
+ 
+#define TOP_BYTE_ARRAY 		0 
+#define BOTTOM_BYTE_ARRAY 	32*16*3 
  
 static struct hrtimer hr_timer; 
+static int newframe=0;
 static int ledpanel_row=0;
-static int pbuffer_top;
-static int pbuffer_bottom=1536;
+static int pbuffer_top=TOP_BYTE_ARRAY;
+static int pbuffer_bottom=BOTTOM_BYTE_ARRAY;
 
 static DEFINE_MUTEX(sysfs_lock);
 static unsigned char rgb_buffer[MAXBUFFER_PER_PANEL];
+static unsigned char rgb_buffer_copy[MAXBUFFER_PER_PANEL];
+
+// 3 bit per color
+#define COLOR_MASK  0x07
+
+// 0xFFFFF400 (PIOA)
+
+#define PA_SODR AT91_IO_P2V(0xFFFFF400+0x30) // PA SODR 
+#define PA_CODR AT91_IO_P2V(0xFFFFF400+0x34) // PA CODR 
+
+// RGB lines for the top side
+#define R0_MASK 1 << 21
+#define G0_MASK 1 << 22
+#define B0_MASK 1 << 23
  
+// RGB lines for the bottom side
+#define R1_MASK 1 << 24
+#define G1_MASK 1 << 25
+#define B1_MASK 1 << 26
+
+// Row address 
+#define A_MASK  1 << 5
+#define B_MASK  1 << 6
+#define C_MASK  1 << 7
+#define D_MASK  1 << 8
+
+// Control signal
+#define CLK_MASK	1 << 27 // Clock (low to hi)
+#define STB_MASK	1 << 28 // Strobe (low to hi) 
+#define OE_MASK		1 << 29 // Output enable (active low)
+
+// Test point line for timing measurements
+#define TP_MASK		1 << 30 
+
+// Faster way I found to manage a GPIO line
+#define R0_HI	*((unsigned long *)PA_SODR) = (unsigned long)(R0_MASK);  
+#define R0_LO	*((unsigned long *)PA_CODR) = (unsigned long)(R0_MASK);  
+#define G0_HI	*((unsigned long *)PA_SODR) = (unsigned long)(G0_MASK);  
+#define G0_LO	*((unsigned long *)PA_CODR) = (unsigned long)(G0_MASK);  
+#define B0_HI	*((unsigned long *)PA_SODR) = (unsigned long)(B0_MASK);  
+#define B0_LO	*((unsigned long *)PA_CODR) = (unsigned long)(B0_MASK);  
+
+#define R1_HI	*((unsigned long *)PA_SODR) = (unsigned long)(R1_MASK);  
+#define R1_LO	*((unsigned long *)PA_CODR) = (unsigned long)(R1_MASK);  
+#define G1_HI	*((unsigned long *)PA_SODR) = (unsigned long)(G1_MASK);  
+#define G1_LO	*((unsigned long *)PA_CODR) = (unsigned long)(G1_MASK);  
+#define B1_HI	*((unsigned long *)PA_SODR) = (unsigned long)(B1_MASK);  
+#define B1_LO	*((unsigned long *)PA_CODR) = (unsigned long)(B1_MASK);  
+
+#define A_HI	*((unsigned long *)PA_SODR) = (unsigned long)(A_MASK);  
+#define A_LO	*((unsigned long *)PA_CODR) = (unsigned long)(A_MASK);  
+#define B_HI	*((unsigned long *)PA_SODR) = (unsigned long)(B_MASK);  
+#define B_LO	*((unsigned long *)PA_CODR) = (unsigned long)(B_MASK);  
+#define C_HI	*((unsigned long *)PA_SODR) = (unsigned long)(C_MASK);  
+#define C_LO	*((unsigned long *)PA_CODR) = (unsigned long)(C_MASK);  
+#define D_HI	*((unsigned long *)PA_SODR) = (unsigned long)(D_MASK);  
+#define D_LO	*((unsigned long *)PA_CODR) = (unsigned long)(D_MASK);  
+
+#define CLK_HI	*((unsigned long *)PA_SODR) = (unsigned long)(CLK_MASK);  
+#define CLK_LO	*((unsigned long *)PA_CODR) = (unsigned long)(CLK_MASK);  
+#define STB_HI	*((unsigned long *)PA_SODR) = (unsigned long)(STB_MASK);  
+#define STB_LO	*((unsigned long *)PA_CODR) = (unsigned long)(STB_MASK);  
+#define OE_HI	*((unsigned long *)PA_SODR) = (unsigned long)(OE_MASK);  
+#define OE_LO	*((unsigned long *)PA_CODR) = (unsigned long)(OE_MASK);  
+
+#define TP_HI	*((unsigned long *)PA_SODR) = (unsigned long)(TP_MASK);  
+#define TP_LO	*((unsigned long *)PA_CODR) = (unsigned long)(TP_MASK);  
+
 // Arietta G25 GPIO lines used
 // http://www.acmesystems.it/pinout_arietta
+// http://www.acmesystems.it/P6LED3232
 // http://www.acmesystems.it/ledpanel
  
 // GPIO lines used 
@@ -63,6 +159,7 @@ static char ledpanel_gpio[] = {
 	27, // CLK
 	28, // STB
 	29, // OE 
+	30, // TP 
 }; 
 
 const char *ledpanel_label[] = {
@@ -76,59 +173,58 @@ const char *ledpanel_label[] = {
 	"B",
 	"C",
 	"D",
-	"OE", 
 	"CLK",
-	"STB"
+	"STB",
+	"OE", 
+	"TP", 
 }; 
 
 // This function is called when you write something on /sys/class/ledpanel/rgb_buffer
 // passing in *buf the incoming content
 
-// rgb_buffer is the content to show on the panel(s) in rgb 8 bit format
+// rgb_buffer is the content to show on the panel(s)
 static ssize_t ledpanel_rgb_buffer(struct class *class, struct class_attribute *attr, const char *buf, size_t len) {
+	int i;
 	mutex_lock(&sysfs_lock);
+	
 	if ((len<=MAXBUFFER_PER_PANEL)) {
 		memset(rgb_buffer,MAXBUFFER_PER_PANEL,0);
 		memcpy(rgb_buffer,buf,len);
 	} else {
 		memcpy(rgb_buffer,buf,MAXBUFFER_PER_PANEL);
 	}		
+
+	for (i=0;i<MAXBUFFER_PER_PANEL;i++) {
+		rgb_buffer[i]>>=5;
+		rgb_buffer[i]&=COLOR_MASK;
+	}	
+
+	memcpy(rgb_buffer_copy,rgb_buffer,MAXBUFFER_PER_PANEL);
+	newframe=1;
+	
 	mutex_unlock(&sysfs_lock);
-	//printk(KERN_INFO "Buffer len %ld bytes\n", len);
+	//printk(KERN_INFO "Buffer len %d bytes\n", len);
+	
+	/*for (i=BOTTOM_BYTE_ARRAY;i<BOTTOM_BYTE_ARRAY+30;i+=3) {
+		printk(KERN_INFO "Banco 2: %02X %02X %02X\n",rgb_buffer[i+0],rgb_buffer[i+1],rgb_buffer[i+2]);
+	}*/		
 	return len;
 }
 	
-// Sysfs definitions for ledpanel class
-// For any name a file in /sys/class/ledpanel is created
+// Definition of the attributes (files created in /sys/class/ledpanel) 
+// used by the ledpanel driver class and related callback function to be 
+// called when someone in user space write on this files
 static struct class_attribute ledpanel_class_attrs[] = {
    __ATTR(rgb_buffer,   0200, NULL, ledpanel_rgb_buffer),
    __ATTR_NULL,
 };
 
-// Name of directory created in /sys/class
+// Name of directory created in /sys/class for this driver
 static struct class ledpanel_class = {
   .name =        "ledpanel",
   .owner =       THIS_MODULE,
   .class_attrs = ledpanel_class_attrs,
 };
-
-// Send on A,B,C,D lines the row address (0 to 15)
-static void ledpanel_set_ABCD(unsigned char address) 
-{
-	gpio_set_value(ledpanel_gpio[LEDPANEL_A],0);
-	gpio_set_value(ledpanel_gpio[LEDPANEL_B],0);
-	gpio_set_value(ledpanel_gpio[LEDPANEL_C],0);
-	gpio_set_value(ledpanel_gpio[LEDPANEL_D],0);
-
-	if (address & 1) 
-		gpio_set_value(ledpanel_gpio[LEDPANEL_A],1);
-	if (address & 2) 
-		gpio_set_value(ledpanel_gpio[LEDPANEL_B],1);
-	if (address & 4) 
-		gpio_set_value(ledpanel_gpio[LEDPANEL_C],1);
-	if (address & 8) 
-		gpio_set_value(ledpanel_gpio[LEDPANEL_D],1);
-} 
 
 // Set the initial state of GPIO lines
 static int ledpanel_gpio_init(void) {
@@ -144,9 +240,14 @@ static int ledpanel_gpio_init(void) {
 		if (rtc!=0) return -1;
     }
 
-    gpio_set_value(ledpanel_gpio[LEDPANEL_OE],1);
+    gpio_set_value(ledpanel_gpio[LEDPANEL_A],0);
+    gpio_set_value(ledpanel_gpio[LEDPANEL_B],0);
+    gpio_set_value(ledpanel_gpio[LEDPANEL_C],0);
+    gpio_set_value(ledpanel_gpio[LEDPANEL_D],0);
     gpio_set_value(ledpanel_gpio[LEDPANEL_CLK],0);
     gpio_set_value(ledpanel_gpio[LEDPANEL_STB],0);
+    gpio_set_value(ledpanel_gpio[LEDPANEL_OE],1);
+    gpio_set_value(ledpanel_gpio[LEDPANEL_TP],0);
 	return 0;
 }
 
@@ -154,42 +255,135 @@ static int ledpanel_gpio_init(void) {
 // Callback function called by the hrtimer
 enum hrtimer_restart ledpanel_hrtimer_callback(struct hrtimer *timer){
 	int col;
-
-	gpio_set_value(ledpanel_gpio[LEDPANEL_OE],1);	
-	ledpanel_set_ABCD(ledpanel_row);
-	for (col=0;col<32;col++) {
-		gpio_set_value(ledpanel_gpio[LEDPANEL_R0],rgb_buffer[pbuffer_top+0]);
-		gpio_set_value(ledpanel_gpio[LEDPANEL_G0],rgb_buffer[pbuffer_top+1]);
-		gpio_set_value(ledpanel_gpio[LEDPANEL_B0],rgb_buffer[pbuffer_top+2]);
-		gpio_set_value(ledpanel_gpio[LEDPANEL_R1],rgb_buffer[pbuffer_bottom+0]);
-		gpio_set_value(ledpanel_gpio[LEDPANEL_G1],rgb_buffer[pbuffer_bottom+1]);
-		gpio_set_value(ledpanel_gpio[LEDPANEL_B1],rgb_buffer[pbuffer_bottom+2]);
-		
-		gpio_set_value(ledpanel_gpio[LEDPANEL_CLK],1);
-		gpio_set_value(ledpanel_gpio[LEDPANEL_CLK],0);
-
-		pbuffer_top+=3;
-		pbuffer_bottom+=3;
-	}		
-	gpio_set_value(ledpanel_gpio[LEDPANEL_STB],1);
-	gpio_set_value(ledpanel_gpio[LEDPANEL_STB],0);
-	gpio_set_value(ledpanel_gpio[LEDPANEL_OE],0);	
 	
-	if ((++ledpanel_row)==16) {
+	TP_HI;
+	
+	if (newframe==1) {
+		newframe=0;
 		ledpanel_row=0;
-		pbuffer_top=0;
-		pbuffer_bottom=1536;
+		pbuffer_top=TOP_BYTE_ARRAY;
+		pbuffer_bottom=BOTTOM_BYTE_ARRAY;
+	}
+   
+	for (col=0;col<32;col++) {
+
+		//RED 0
+		if (rgb_buffer_copy[pbuffer_top]==0) {
+			*((unsigned long *)PA_CODR) = R0_MASK;
+		} else {	
+			if (rgb_buffer_copy[pbuffer_top]<=rgb_buffer[pbuffer_top]) {
+				*((unsigned long *)PA_SODR) = R0_MASK;
+			}
+		}
+		if (rgb_buffer[pbuffer_top]>0) {
+			rgb_buffer_copy[pbuffer_top]--;
+			rgb_buffer_copy[pbuffer_top]&=COLOR_MASK;
+		}
+		pbuffer_top++;
+		
+		//GREEN 0
+		if (rgb_buffer_copy[pbuffer_top]==0) {
+			*((unsigned long *)PA_CODR) = G0_MASK;
+		} else {	
+			if (rgb_buffer_copy[pbuffer_top]<=rgb_buffer[pbuffer_top]) {
+				*((unsigned long *)PA_SODR) = G0_MASK;
+			}
+		}
+		if (rgb_buffer[pbuffer_top]>0) {
+			rgb_buffer_copy[pbuffer_top]--;
+			rgb_buffer_copy[pbuffer_top]&=COLOR_MASK;
+		}
+		pbuffer_top++;
+
+		//BLUE 0
+		if (rgb_buffer_copy[pbuffer_top]==0) {
+			*((unsigned long *)PA_CODR) = B0_MASK;
+		} else {	
+			if (rgb_buffer_copy[pbuffer_top]<=rgb_buffer[pbuffer_top]) {
+				*((unsigned long *)PA_SODR) = B0_MASK;
+			}
+		}
+		if (rgb_buffer[pbuffer_top]>0) {
+			rgb_buffer_copy[pbuffer_top]--;
+			rgb_buffer_copy[pbuffer_top]&=COLOR_MASK;
+		}
+		pbuffer_top++;
+
+		//RED 1
+		if (rgb_buffer_copy[pbuffer_bottom]==0) {
+			*((unsigned long *)PA_CODR) = R1_MASK;
+		} else {	
+			if (rgb_buffer_copy[pbuffer_bottom]<=rgb_buffer[pbuffer_bottom]) {
+				*((unsigned long *)PA_SODR) = R1_MASK;
+			}
+		}
+		if (rgb_buffer[pbuffer_bottom]>0) {
+			rgb_buffer_copy[pbuffer_bottom]--;
+			rgb_buffer_copy[pbuffer_bottom]&=COLOR_MASK;
+		}
+		pbuffer_bottom++;
+
+		//GREEN 1
+		if (rgb_buffer_copy[pbuffer_bottom]==0) {
+			*((unsigned long *)PA_CODR) = G1_MASK;
+		} else {	
+			if (rgb_buffer_copy[pbuffer_bottom]<=rgb_buffer[pbuffer_bottom]) {
+				*((unsigned long *)PA_SODR) = G1_MASK;
+			}
+		}
+		if (rgb_buffer[pbuffer_bottom]>0) {
+			rgb_buffer_copy[pbuffer_bottom]--;
+			rgb_buffer_copy[pbuffer_bottom]&=COLOR_MASK;
+		}
+		pbuffer_bottom++;
+
+		//BLUE 1
+		if (rgb_buffer_copy[pbuffer_bottom]==0) {
+			*((unsigned long *)PA_CODR) = B1_MASK;
+		} else {	
+			if (rgb_buffer_copy[pbuffer_bottom]<=rgb_buffer[pbuffer_bottom]) {
+				*((unsigned long *)PA_SODR) = B1_MASK;
+			}
+		}
+		if (rgb_buffer[pbuffer_bottom]>0) {
+			rgb_buffer_copy[pbuffer_bottom]--;
+			rgb_buffer_copy[pbuffer_bottom]&=COLOR_MASK;
+		}
+		pbuffer_bottom++;
+
+		CLK_HI;
+		CLK_LO;
+	}		
+
+	// Disable the OE
+    gpio_set_value(ledpanel_gpio[LEDPANEL_OE],1);
+
+	// Change A,B,C,D
+	*((unsigned long *)PA_CODR) = (0xF << 5);
+	*((unsigned long *)PA_SODR) = (ledpanel_row << 5);
+
+    gpio_set_value(ledpanel_gpio[LEDPANEL_STB],1);
+	gpio_set_value(ledpanel_gpio[LEDPANEL_STB],0);
+	gpio_set_value(ledpanel_gpio[LEDPANEL_OE],0);
+	
+	ledpanel_row++;
+	if (ledpanel_row>=16) {
+		ledpanel_row=0;
+		pbuffer_top=TOP_BYTE_ARRAY;
+		pbuffer_bottom=BOTTOM_BYTE_ARRAY;
 	}
 
-	hrtimer_start(&hr_timer, ktime_set(0,0), HRTIMER_MODE_REL);
-	return HRTIMER_NORESTART;
+	TP_LO;
+	hrtimer_start(&hr_timer, ktime_set(0,25000), HRTIMER_MODE_REL);
+	//hrtimer_start(&hr_timer, ktime_set(0,100000000), HRTIMER_MODE_REL);
+ 	return HRTIMER_NORESTART;
 }
 
 static int ledpanel_init(void)
 {
 	struct timespec tp;
 	
-    printk(KERN_INFO "Ledpanel driver v1.01 initializing.\n");
+    printk(KERN_INFO "Ledpanel (pwm) driver v1.00 - initializing.\n");
 
 	if (class_register(&ledpanel_class)<0) goto fail;
     
@@ -206,7 +400,7 @@ static int ledpanel_init(void)
 	
 	hrtimer_init(&hr_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
 	hr_timer.function = &ledpanel_hrtimer_callback;
-	hrtimer_start(&hr_timer, ktime_set(0,200000), HRTIMER_MODE_REL);
+	hrtimer_start(&hr_timer, ktime_set(0,0), HRTIMER_MODE_REL);
 	return 0;
 
 fail:
