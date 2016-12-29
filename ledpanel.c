e2f0824afc2480c15456aff46be85944a1dc8a7d
Andrea Montefusco IW0HDV <andrew@montefusco.com>
2016-12-29 16:40:12 +0100

    Multi module capability.

--- ledpanel.c
+++ ledpanel.c
@@ -14,6 +14,8 @@
  * Alexandre Belloni <alexandre.belloni@free-electrons.com>
  * for his contribute
  *
+ * Multiple panel support by Andrea Montefusco (andrew@montefusco.com)
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
@@ -35,10 +37,14 @@
 #include <linux/irqflags.h>
 #include <linux/random.h>
 #include <linux/string.h>
+#include <linux/slab.h>
+#include <linux/version.h>
 
 #include <asm/io.h>
 
-#define MAXBUFFER_PER_PANEL 32*32*3
+#define MOD_W 32
+#define MOD_H 32
+#define BRIGHTNESS_LEVEL    16
 
 MODULE_LICENSE("Dual BSD/GPL");
 
@@ -46,15 +52,17 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Sergio Tanzilli");
 MODULE_DESCRIPTION("Bit banging driver for RGB 32x32 LED PANEL");
 
-#define BRIGHTNESS_LEVEL    16
+static struct hrtimer hr_timer;   // scheduler for panel refresh
+static int ledpanel_row=0;        // used in panel refresh
 
-static struct hrtimer hr_timer;
-static int ledpanel_row=0;
+static unsigned char *rgb_buffer; // [MAXBUFFER_PER_PANEL];
+static unsigned char *pwm_buffer; // [64*16*BRIGHTNESS_LEVEL];
+static int rgb_buffer_size = 0;
+static int rgb_buffer_len  = 0;
+static int pwm_buffer_size = 0;
 
-static DEFINE_MUTEX(sysfs_lock);
-static unsigned char rgb_buffer[MAXBUFFER_PER_PANEL];
-static unsigned char pwm_buffer[32*16*BRIGHTNESS_LEVEL];
 static int pwm_buffer_index=0;
+
 
 // 0xFFFFF400 (PIOA)
 #define PIO_PER		0x00	/* Enable Register */
@@ -90,85 +98,9 @@ static void __iomem *pioa;
 #define ALLWHITE_MASK	(R0_MASK | G0_MASK | B0_MASK | R1_MASK | G1_MASK | B1_MASK)
 #define ALLBLACK_MASK	~(R0_MASK | G0_MASK | B0_MASK | R1_MASK | G1_MASK | B1_MASK)
 
-// This function is called when you write the rgb contents on /sys/class/ledpanel/rgb_buffer
-// To improve the speed the rgb buffer is converted in a stream of data ready to send
-// to the LCD pannel to implements the brightness control
 
-static ssize_t ledpanel_rgb_buffer(struct class *class, struct class_attribute *attr, const char *buf, size_t len) {
-	int i;
-	int index_pwm, pwm_panel;
-
-	//mutex_lock(&sysfs_lock);
-
-	//rgb buffer clean up
-	memset(rgb_buffer,0,MAXBUFFER_PER_PANEL);
-
-	//pwm buffer clean up
-	memset(pwm_buffer,0,32*16*BRIGHTNESS_LEVEL);
-
-	if ((len<=MAXBUFFER_PER_PANEL)) {
-		memcpy(rgb_buffer,buf,len);
-	} else {
-		memcpy(rgb_buffer,buf,MAXBUFFER_PER_PANEL);
-	}
-	//printk(KERN_INFO "Buffer len %d bytes\n", len);
-
-	// Convert the received RGB buffer in PWM buffer
-
-	// RGB buffer is the image to show in RGB 8+8+8 format
-
-	// | 1 byte red | 1 byte green | 1 byte blue | X 32 columns x 32 rows
-
-	// PWM buffer contain the "scene" to send to the led matrix.
-	// Each byte contains | -- -- B1 G1 R1 B0 G0 R0 |
-	// Each scene is 512 bytes lenght
-	// (32x32 (leds) x3 (color) / 6 (line for 2 leds) = 512)
-
-	// To have 16 brightness level this driver sents to the panel
-	// a stream of 16 scene so 512x16 = 8.192 byte
-
-	index_pwm=0;
-	for (pwm_panel=0;pwm_panel<BRIGHTNESS_LEVEL;pwm_panel++) {
-		for (i=0;i<1536;i+=3) {
-			// R0
-			if (rgb_buffer[i+0]>0) {
-				pwm_buffer[index_pwm]|=0x01;
-				rgb_buffer[i+0]--;
-			}
-			// G0
-			if (rgb_buffer[i+1]>0) {
-				pwm_buffer[index_pwm]|=0x02;
-				rgb_buffer[i+1]--;
-			}
-			// B0
-			if (rgb_buffer[i+2]>0) {
-				pwm_buffer[index_pwm]|=0x04;
-				rgb_buffer[i+2]--;
-			}
-
-			// R1
-			if (rgb_buffer[i+0+1536]>0) {
-				pwm_buffer[index_pwm]|=0x08;
-				rgb_buffer[i+0+1536]--;
-			}
-			// G1
-			if (rgb_buffer[i+1+1536]>0) {
-				pwm_buffer[index_pwm]|=0x10;
-				rgb_buffer[i+1+1536]--;
-			}
-			// B1
-			if (rgb_buffer[i+2+1536]>0) {
-				pwm_buffer[index_pwm]|=0x20;
-				rgb_buffer[i+2+1536]--;
-			}
-			index_pwm++;
-		}
-	}
-	//mutex_unlock(&sysfs_lock);
-	ledpanel_row=0;
-	pwm_buffer_index=0;
-	return len;
-}
+// callback function forward declaration
+static ssize_t ledpanel_rgb_buffer(struct class *class, struct class_attribute *attr, const char *buf, size_t len);
 
 // Definition of the attributes (files created in /sys/class/ledpanel)
 // used by the ledpanel driver class and related callback function to be
@@ -185,11 +117,119 @@ static struct class ledpanel_class = {
   .class_attrs = ledpanel_class_attrs,
 };
 
+static int nmodule = 1 ;
+module_param(nmodule, int, 0644);
+MODULE_PARM_DESC(nmodule, "How many modules are attached.");
+
+static int width = MOD_W ;
+module_param(width, int, 0444);
+MODULE_PARM_DESC(width, "Panel width in pixels.");
+
+static int height = MOD_H ;
+module_param(height, int, 0444);
+MODULE_PARM_DESC(height, "Panel height in pixels.");
+
+
+
+// This function is called when you write the rgb contents on /sys/class/ledpanel/rgb_buffer
+// To improve the speed the rgb buffer is converted in a stream of data ready to send
+// to the LCD pannel to implements the brightness control
+
+static ssize_t ledpanel_rgb_buffer(struct class *class, struct class_attribute *attr, const char *buf, size_t len) {
+
+//printk(KERN_INFO "len: %d ofs:%d\n", len, ofs);
+
+
+	if ((len <= (rgb_buffer_size - rgb_buffer_len))) {
+		memcpy(rgb_buffer+rgb_buffer_len,buf,len);
+	} else {
+		memcpy(rgb_buffer+rgb_buffer_len,buf, rgb_buffer_size-rgb_buffer_len);
+	}
+	rgb_buffer_len += len;
+
+	if (rgb_buffer_len >= rgb_buffer_size) {
+		int index_pwm=0;
+		int pwm_panel, i;
+
+		rgb_buffer_len = 0;
+
+		//printk(KERN_INFO "Buffer len %d bytes\n", len);
+
+		// Convert the received RGB buffer in PWM buffer
+
+		// RGB buffer is the image to show in RGB 8+8+8 format
+
+		// | 1 byte red | 1 byte green | 1 byte blue | X 32 columns x 32 rows
+
+		// PWM buffer contain the "scene" to send to the led matrix.
+		// Each byte contains | -- -- B1 G1 R1 B0 G0 R0 |
+		// Each scene is 512 bytes lenght
+		// (32x32 (leds) x3 (color) / 6 (line for 2 leds) = 512)
+
+		// To have 16 brightness level this driver sents to the panel
+		// a stream of 16 scene so 512x16 = 8.192 byte
+
+		memset(pwm_buffer, 0, pwm_buffer_size);
+
+		for (pwm_panel=0;pwm_panel<BRIGHTNESS_LEVEL;pwm_panel++) {
+			for (i=0; i < ((width*height*3)/2); i+=3) {
+				// R0
+				if (rgb_buffer[i+0]>0) {
+					pwm_buffer[index_pwm]|=0x01;
+					rgb_buffer[i+0]--;
+				}
+				// G0
+				if (rgb_buffer[i+1]>0) {
+					pwm_buffer[index_pwm]|=0x02;
+					rgb_buffer[i+1]--;
+				}
+				// B0
+				if (rgb_buffer[i+2]>0) {
+					pwm_buffer[index_pwm]|=0x04;
+					rgb_buffer[i+2]--;
+				}
+
+				// R1
+				if (rgb_buffer[i+0+((width*height*3)/2)]>0) {
+					pwm_buffer[index_pwm]|=0x08;
+					rgb_buffer[i+0+((width*height*3)/2)]--;
+				}
+				// G1
+				if (rgb_buffer[i+1+((width*height*3)/2)]>0) {
+					pwm_buffer[index_pwm]|=0x10;
+					rgb_buffer[i+1+((width*height*3)/2)]--;
+				}
+				// B1
+				if (rgb_buffer[i+2+((width*height*3)/2)]>0) {
+					pwm_buffer[index_pwm]|=0x20;
+					rgb_buffer[i+2+((width*height*3)/2)]--;
+				}
+				index_pwm++;
+			}
+		}
+		//mutex_unlock(&sysfs_lock);
+		ledpanel_row=0;
+		pwm_buffer_index=0;
+	}
+	return len;
+}
+
+
+/*
+* Assume interrupts are turned off.
+* Length of each clock cycle = (1 / ProcessorFrequency)
+*/
+__inline__ void nop_sleep(unsigned long clk_cycs) {
+      while (clk_cycs-- > 0)
+            __asm__ volatile ("nop;");
+}
+
+
 // Send a new row the panel
 // Callback function called by the hrtimer each 67uS
 enum hrtimer_restart ledpanel_hrtimer_callback(struct hrtimer *timer){
 	int col;
-	for (col=0;col<32;col++) {
+	for (col=0; col < width; col++) {
 		writel_relaxed(ALLWHITE_MASK, pioa + PIO_CODR);
 
 		//Test all led white
@@ -226,7 +266,7 @@ enum hrtimer_restart ledpanel_hrtimer_callback(struct hrtimer *timer){
 	// writel_relaxed(R0_MASK, pioa + PIO_CODR);
 	// writel_relaxed(R0_MASK, pioa + PIO_SODR);
 
-	if (pwm_buffer_index>=(512*BRIGHTNESS_LEVEL))
+	if (pwm_buffer_index>= pwm_buffer_size)
 		pwm_buffer_index=0;
 
 	// Enable OE
@@ -238,14 +278,26 @@ enum hrtimer_restart ledpanel_hrtimer_callback(struct hrtimer *timer){
 
 static int ledpanel_init(void)
 {
-	struct timespec tp;
+	int rc;
 
-    printk(KERN_INFO "Ledpanel driver 1.00 - initializing.\n");
+	width = MOD_W * nmodule ;
 
-	if (class_register(&ledpanel_class)<0) goto fail;
+	printk(KERN_INFO "Ledpanel driver 2.00 - initializing.\n");
 
-	hrtimer_get_res(CLOCK_MONOTONIC, &tp);
-	printk(KERN_INFO "Clock resolution: %ldns\n", tp.tv_nsec);
+	if ((rc=class_register(&ledpanel_class))<0) goto fail;
+
+	rgb_buffer_size = width * height *3;
+	rgb_buffer = kmalloc (rgb_buffer_size, GFP_KERNEL);
+	if (rgb_buffer == 0) goto fail_nomem;
+
+	pwm_buffer_size = width * (height/2) * BRIGHTNESS_LEVEL;
+	pwm_buffer = kmalloc (pwm_buffer_size, GFP_KERNEL);
+	if (pwm_buffer == 0) goto fail_nomem;
+
+	//rgb buffer clean up
+	memset(rgb_buffer,0,rgb_buffer_size);
+	//pwm buffer clean up
+	memset(pwm_buffer,0,pwm_buffer_size);
 
 	pioa = ioremap(0xFFFFF400, 0x200);
 	writel_relaxed(ALL_MASK, pioa + PIO_PER);
@@ -253,19 +305,34 @@ static int ledpanel_init(void)
 	writel_relaxed(ALL_MASK, pioa + PIO_CODR);
 	writel_relaxed(OE_MASK, pioa + PIO_SODR);
 
+	printk(KERN_INFO "Ledpanel driver 2.00 - width: %d height: %d.\n", width, height);
+
 	printk(KERN_INFO "Brightness levels: %d\n", BRIGHTNESS_LEVEL);
-	printk(KERN_INFO "RGB buffer lenght: %d bytes\n", MAXBUFFER_PER_PANEL);
-	printk(KERN_INFO "PWM buffer lenght: %d bytes\n", 32*16*BRIGHTNESS_LEVEL);
+	printk(KERN_INFO "RGB buffer lenght: %d bytes\n", rgb_buffer_size);
+	printk(KERN_INFO "PWM buffer lenght: %d bytes\n", pwm_buffer_size);
 
 	hrtimer_init(&hr_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
 	hr_timer.function = &ledpanel_hrtimer_callback;
 	hrtimer_start(&hr_timer, ktime_set(0,0), HRTIMER_MODE_REL);
+
+	#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,2,0)
+	printk(KERN_INFO "Clock resolution: %ldns\n", LOW_RES_NSEC);
+	#else
+	{
+		struct timespec tp;
+		hrtimer_get_res(CLOCK_MONOTONIC, &tp);
+		printk(KERN_INFO "Clock resolution: %ldns\n", tp.tv_nsec);
+	}
+	#endif
+
 	return 0;
 
 fail:
-	printk(KERN_INFO "Ledpanel error\n");
+	printk(KERN_INFO "Ledpanel error: %d\n", rc);
 	return -1;
-
+fail_nomem:
+	printk(KERN_INFO "Ledpanel error: no enough memory.\n");
+	return -1;
 }
 
 static void ledpanel_exit(void)
@@ -274,7 +341,9 @@ static void ledpanel_exit(void)
 	writel_relaxed(OE_MASK, pioa + PIO_SODR);
 	iounmap(pioa);
 	class_unregister(&ledpanel_class);
-    printk(KERN_INFO "Ledpanel disabled.\n");
+	if (rgb_buffer) kfree(rgb_buffer);
+	if (pwm_buffer) kfree(pwm_buffer);
+	printk(KERN_INFO "Ledpanel disabled.\n");
 }
 
 module_init(ledpanel_init);
